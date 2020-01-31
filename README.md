d6b90394137d587da26924d5361a94cd22067317
Fabrice <fareg.colas@gmail.com>
2020-01-31 18:09:00 +0100

    Added: build-last-tmux-from-sources.sh script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Linux tools
+
+## Build last ```tmux``` version
+
+Build last ```tmux``` version from sources and install it on the local machine
+
+```bash
+curl "https://raw.githubusercontent.com/fareg/lnx-tools/master/build-last-tmux-from-sources.sh?raw=true" 2>/dev/null | sudo sh
+```
---
+++ build-last-tmux-from-sources.sh
@@ -0,0 +1,12 @@
+# Build last tmux version and install it on the local machine
+apt update
+apt install -y git automake build-essential pkg-config libevent-dev libncurses5-dev bison
+pushd .
+cd /tmp
+rm -fr /tmp/tmux
+git clone https://github.com/tmux/tmux.git /tmp/tmux
+cd /tmp/tmux
+git checkout master
+sh autogen.sh && ./configure && make && make install
+popd 
+rm -fr /tmp/tmux
